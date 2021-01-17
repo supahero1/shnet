9e8581ca3ba1f5ccf2589d56769366281b508e7a
shädam <47268949+supahero1@users.noreply.github.com>
2021-01-17 15:46:26 +0100

    Update net.h

--- src/net.h
+++ src/net.h
@@ -1,19 +1,3 @@
-/*
-   Copyright 2021 shädam
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 #ifndef W_pOOj__2VkXM_ew9zpRf2pZH_H8ggTe
 #define W_pOOj__2VkXM_ew9zpRf2pZH_H8ggTe 1
 
