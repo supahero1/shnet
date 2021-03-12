c5785b3d90837dad6414d58a0b92caa8c3229f66
shädam <47268949+supahero1@users.noreply.github.com>
2021-03-12 16:46:16 +0100

    Update net_avl.h

--- src/net_avl.h
+++ src/net_avl.h
@@ -1,19 +1,3 @@
-/*
-   Copyright 2021 shädam
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 #ifndef lu_eoVE_9ZX_B__QSYe_uZ2d_kgw_b_f
 #define lu_eoVE_9ZX_B__QSYe_uZ2d_kgw_b_f 1
 
@@ -21,7 +5,7 @@
 extern "C" {
 #endif
 
-#include "../def.h"
+#include "def.h"
 
 #include <stdint.h>
 
