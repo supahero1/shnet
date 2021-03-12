012f55ca2a0407359e06447706a2ce2adbbf0a84
shädam <47268949+supahero1@users.noreply.github.com>
2021-03-12 16:44:38 +0100

    Update net.c

--- src/net.c
+++ src/net.c
@@ -1,93 +1,19 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
 #include <pthread.h>
+#include <sys/epoll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/syscall.h>
 
 #include "net.h"
+#include "net_avl.h"
 
 #include <stdio.h>
-#include <string.h>
-#include <arpa/inet.h>
-
-/*int ParseHTTP_URL(const char* const u, struct HTTP_URL* const restrict url, const enum HTTP_URL_FLAGS flags) {
-  unsigned int i = 0;
-  unsigned int j = 0;
-  const char* a = u;
-  const char* b;
-  const char* c;
-  const char* d;
-  const size_t len = strlen(u);
-  // these flags are so useless
-  const int PPR = flags & NET_PPR; // parse protocol
-  const int PHO = flags & NET_PHO; // parse host
-  const int POR = flags & NET_POR; // parse origin
-  const int PPO = flags & NET_PPO; // parse port
-  const int PPA = flags & NET_PPA; // parse pathname
-  const int PQU = flags & NET_PQU; // parse query
-  const int PHA = flags & NET_PHA; // parse hash
-  
-  if(PPR != 0) {
-    if(u[4] == 's') {
-      url->protocol = "https";
-      url->protocol_len = 5;
-    } else {
-      url->protocol = "http";
-      url->protocol_len = 4;
-    }
-  }
-  if(PPO != 0) {
-    if(u[7] == '[') {
-      b = strchr(strchr(u + 11, ']'), ':');
-    } else if(u[8] == '[') {
-      b = strchr(strchr(u + 12, ']'), ':');
-    } else {
-      b = strchr(u + 10, ':');
-    }
-    if(b == NULL) {
-      url->port = "80";
-      url->port_len = 2;
-    } else if(b[1] == '/' || b[1] == '\0') {
-      url->port = "80";
-      url->port_len = 2;
-    } else {
-      url->port = b + 1;
-      if(b[2] == '/' || b[2] == '\0') {
-        url->port_len = 1;
-      } else if(b[3] == '/' || b[3] == '\0') {
-        url->port_len = 2;
-      } else if(b[4] == '/' || b[4] == '\0') {
-        url->port_len = 3;
-      } else if(b[5] == '/' || b[5] == '\0') {
-        url->port_len = 4;
-      } else {
-        url->port_len = 5;
-      }
-    }
-  }
-  if(PHO != 0) {
-    if(u[4] == 's') {
-      url->host = u + 8;
-    } else {
-      url->host = u + 7;
-    }
-    if(b != NULL) {
-      url->host_len = (uintptr_t) b - (uintptr_t) a;
-    } else {
-      c = strchr(a + 1, "/");
-      if(c != NULL) {
-        url->host_len = (uintptr_t) c - (uintptr_t) a;
-      } else {
-        url->host_len = (uintptr_t) url + len - (uintptr_t) a;
-      }
-    }
-  }
-  if(POR != 0) {
-    
-  }
-}*/
+#include <arpa/inet.h> // htons, htonl, etc
 
 int GetAddrInfo(const char* const hostname, const char* const service, const int flags, struct addrinfo** const restrict res) {
   return getaddrinfo(hostname, service, &((struct addrinfo) {
@@ -133,10 +59,6 @@ static void* AsyncTCPConnectThread(void* a) {
   -2 = connect err
   -3 = no socket succeeded
   */
-  /*(void) sigaction(SIGIO, &((struct sigaction) {
-    .sa_flags = 0,
-    .sa_handler = log // add log function see what happens
-  }), NULL);*/
   for(; i < arr->count; ++i) {
     if(type == NET_GAI) {
       err = GetAddrInfo(arr->infos.gai[i].hostname, arr->infos.gai[i].service, arr->infos.gai[i].flags, &res);
@@ -187,16 +109,13 @@ static void* AsyncTCPConnectThread(void* a) {
         }
         continue;
       } else {
-        err = fcntl(sfd, F_SETFL, fcntl(sfd, F_GETFL, 0) | O_NONBLOCK);
+        err = fcntl(sfd, F_GETFL, 0);
+        if(err == -1) {
+          err = 0;
+        }
+        err = fcntl(sfd, F_SETFL, err | O_NONBLOCK);
         if(err == -1) {
           printf("err at first fcntl: %s\n", strerror(errno));
-        }
-        err = fcntl(sfd, F_SETOWN_EX, &((struct f_owner_ex) {
-          .type = F_OWNER_TID,
-          .pid = syscall(SYS_gettid)
-        }));
-        if(err == -1) {
-          printf("err at second fcntl: %s\n", strerror(errno));
         }
         nets.state = NET_OPEN;
         if(type == NET_GAI) {
@@ -229,10 +148,6 @@ static void* AsyncTCPListenThread(void* a) {
   -3 = listen err
   -3 = no socket succeeded
   */
-  /*(void) sigaction(SIGIO, &((struct sigaction) {
-    .sa_flags = 0,
-    .sa_handler = log // add log function see what happens
-  }), NULL);*/
   for(; i < arr->count; ++i) {
     if(type == NET_GAI) {
       err = GetAddrInfo(arr->infos.gai[i].hostname, arr->infos.gai[i].service, arr->infos.gai[i].flags | AI_PASSIVE, &res);
@@ -293,16 +208,13 @@ static void* AsyncTCPListenThread(void* a) {
         }
         continue;
       } else {
-        err = fcntl(sfd, F_SETFL, fcntl(sfd, F_GETFL, 0) | O_NONBLOCK);
+        err = fcntl(sfd, F_GETFL, 0);
+        if(err == -1) {
+          err = 0;
+        }
+        err = fcntl(sfd, F_SETFL, err | O_NONBLOCK);
         if(err == -1) {
           printf("err at first fcntl: %s\n", strerror(errno));
-        }
-        err = fcntl(sfd, F_SETOWN_EX, &((struct f_owner_ex) {
-          .type = F_OWNER_TID,
-          .pid = syscall(SYS_gettid)
-        }));
-        if(err == -1) {
-          printf("err at second fcntl: %s\n", strerror(errno));
         }
         nets.state = NET_OPEN;
         if(type == NET_GAI) {
@@ -320,6 +232,96 @@ static void* AsyncTCPListenThread(void* a) {
 
 #undef arr
 
+static void dummy_signal_handler(__unused int sig) {}
+
+#define manager ((struct NETConnectionManager*) s)
+
+__nonnull((1))
+static void* EPollThread(void* s) {
+  sigset_t mask;
+  struct epoll_event events[100];
+  int err;
+  int i;
+  sigemptyset(&mask);
+  sigfillset(&mask);
+  (void) pthread_sigmask(SIG_BLOCK, &mask, NULL);
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGRTMAX);
+  (void) pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
+  (void) sigaction(SIGRTMAX, &((struct sigaction) {
+    .sa_flags = 0,
+    .sa_handler = dummy_signal_handler
+  }), NULL);
+  while(1) {
+    puts("epoll is waiting");
+    err = epoll_wait(manager->epoll, events, 100, -1);
+    if(err == -1) {
+      printf("epoll got -1 code, errno: %d | %s\n", errno, strerror(errno));
+      close(manager->epoll);
+      net_avl_free(&manager->avl_tree);
+      return NULL;
+    }
+    for(i = 0; i < err; ++i) {
+      net_avl_search(&manager->avl_tree, events[i].data.fd, events[i].events);
+    }
+  }
+  return NULL;
+}
+
+#undef manager
+
+int InitConnectionManager(struct NETConnectionManager* const manager, const uint32_t avg_size) {
+  int epoll = epoll_create1(0);
+  if(epoll == -1) {
+    return errno;
+  }
+  int err = pthread_create(&manager->thread, NULL, EPollThread, manager);
+  if(err != 0) {
+    close(epoll);
+    return err;
+  }
+  net_avl_tree(&manager->avl_tree, avg_size);
+  err = net_avl_init(&manager->avl_tree);
+  if(err != 0) {
+    close(epoll);
+  } else {
+    manager->epoll = epoll;
+  }
+  return err;
+}
+
+int AddSocket(struct NETConnectionManager* const manager, const int sfd, void (*callback)(int, uint32_t)) {
+  int err = net_avl_insert(&manager->avl_tree, sfd, callback);
+  if(err != 0) {
+    puts("failed to add a socket");
+    return err;
+  }
+  err = epoll_ctl(manager->epoll, EPOLL_CTL_ADD, sfd, &((struct epoll_event) {
+    .events = EPOLLIN | EPOLLRDHUP,
+    .data = (epoll_data_t) {
+      .fd = sfd
+    }
+  }));
+  if(err != 0) {
+    net_avl_delete(&manager->avl_tree, sfd);
+    return errno;
+  }
+  return 0;
+}
+
+int DeleteSocket(struct NETConnectionManager* const manager, const int sfd) {
+  net_avl_delete(&manager->avl_tree, sfd);
+  int err = epoll_ctl(manager->epoll, EPOLL_CTL_DEL, sfd, (struct epoll_event*) manager);
+  if(err != 0) {
+    return errno;
+  }
+  return 0;
+}
+
+void FreeConnectionManager(struct NETConnectionManager* const manager) {
+  (void) pthread_sigqueue(manager->thread, SIGRTMAX, (union sigval) { .sival_ptr = NULL });
+}
+
 int AsyncGetAddrInfo(struct AsyncGAIArray* const array) {
   pthread_t t;
   return pthread_create(&t, NULL, AsyncGAIThread, array);
@@ -334,98 +336,3 @@ int AsyncTCPListen(struct AsyncNETArray* const array) {
   pthread_t t;
   return pthread_create(&t, NULL, AsyncTCPListenThread, array);
 }
-
-// old code below, really old, before work distributor
-/*int get_sent_buffered_amount(int sockfd) {
-  int value;
-  (void) ioctl(sockfd, SIOCOUTQ, &value);
-  return value;
-}
-int get_received_buffered_amount(int sockfd) {
-  int value;
-  (void) ioctl(sockfd, SIOCINQ, &value);
-  return value;
-}
-
-int set_cork(int sockfd, const int val) {
-  return setsockopt(sockfd, IPPROTO_TCP, TCP_CORK, &val, sizeof(int));
-}
-
-static int httpsocket(struct addrinfo* const res, pid_t pid) {
-  struct addrinfo* cpy;
-  int sfd;
-  int n = 1;
-  for(cpy = res; cpy != NULL; cpy = cpy->ai_next) {
-    sfd = socket(cpy->ai_family, SOCK_SEQPACKET | SOCK_NONBLOCK, 0);
-    if(sfd == -1) {
-      puts("Couldnt establish socket");
-      continue;
-    }
-    (void) setsockopt(sfd, IPPROTO_TCP, TCP_NODELAY, &n, sizeof(int));
-    (void) ioctl(sfd, FIOASYNC, &n);
-    (void) ioctl(sfd, SIOCSPGRP, &pid);
-    (void) fcntl(sfd, F_SETSIG, &pid);
-    puts("Got a socket");
-    
-    if(connect(sfd, cpy->ai_addr, cpy->ai_addrlen) == 0) {
-      puts("Got a connection!");
-      printf("Our connection has %d mem cached\n", get_sent_buffered_amount(sfd));
-      break;
-    } else {
-      puts("no connection with errno:");
-      printerrno();
-    }
-    close(sfd);
-  }
-  freeaddrinfo(res);
-  return sfd;
-}
-static int httpserver(struct addrinfo* res, const int flags) {
-  struct addrinfo* cpy;
-  int sfd;
-  int yes = 1;
-  for(cpy = res; cpy != NULL; cpy = cpy->ai_next) {
-    sfd = socket(cpy->ai_family, SOCK_STREAM | SOCK_NONBLOCK, 0);
-    if(sfd == -1) {
-      continue;
-    }
-    (void) setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, &yes);
-    (void) setsockopt(sfd, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(int));
-    (void) ioctl(sfd, FIOASYNC, &n);
-    (void) ioctl(sfd, SIOCSPGRP, &pid);
-    if(bind(sfd, cpy->ai_addr, cpy->ai_addrlen) == 0) {
-      break;
-    }
-    close(sfd);
-  }
-  freeaddrinfo(res);
-  if(cpy == NULL) {
-    return -1;
-  }
-  return sfd;
-}
-
-int IPConnect(const char hostname[], const char port[], const int family, pid_t pid) {
-  struct addrinfo* res;
-  int err = gai(res, hostname, AI_NUMERICHOST, port, family, 0);
-  if(err != 0) {
-    puts("failed to get info");
-    return err;
-  }
-  puts("got info");
-  return httpsocket(res, pid);
-}
-int HostConnect(const char hostname[], const char port[], const int family, pid_t pid) {
-  struct addrinfo* res;
-  int err = gai(res, hostname, 0, port, family, 0);
-  if(err != 0) {
-    puts("failed to get info");
-    return err;
-  }
-  puts("got info");
-  return httpsocket(res, pid);
-}
-
-int ServerBind(const char port[], const int family, pid_t pid) {
-  return httpserver(gai(NULL, 0, port, family, AI_PASSIVE), pid);
-}*/
