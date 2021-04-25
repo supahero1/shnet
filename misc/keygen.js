5be33d4d37f3cb0cf8bfc07a292b47dfe4b85dc0
supahero1 <franek.balcerak@o2.pl>
2021-04-25 12:52:42 +0200

    remove old code

--- misc/keygen.js
+++
@@ -1,6 +0,0 @@
-let a = '';
-let b = '_abcd_efgh_ijkl_mnop_qrst_uvwx_yzAB_CDEF_GHIJ_KLMN_OPQR_STUV_WXYZ_0123_4567_89';
-for(let i = 0; i < 32; ++i) {
-  a += b[(Math.random() * b.length) | 0];
-}
-a;
--- misc/keygen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# if the system denies permission to execute this file, do: chmod 755 keygen.sh
-cc keygen.c -o keygen_TysKJnDR_CL0RWn_y5Lhq8z_gr_GwH_f # to not collide with any existing file
-./keygen_TysKJnDR_CL0RWn_y5Lhq8z_gr_GwH_f
-rm keygen_TysKJnDR_CL0RWn_y5Lhq8z_gr_GwH_f
