4b5ea8258aec433cac2b47576122ae5f01b5a4b2
supahero1 <franek.balcerak@o2.pl>
2021-08-07 19:09:18 +0200

    v1.1.2-alpha

--- src/debug.c
+++ src/debug.c
@@ -31,7 +31,7 @@ void _debug(const char* fmt, const int console_log, ...) {
   (void) clock_gettime(CLOCK_REALTIME, &tp);
   const uint64_t t = tp.tv_sec * 1000000000 + tp.tv_nsec - start_time;
   const uint64_t secs = t / 1000000000;
-  fprintf(log_file, "[%4lud %4luh %4lus %5lums %5luus] ", secs / 86400, (secs / 3600) % 24, secs % 3600, (t / 1000000) % 1000, (t / 1000) % 1000);
+  fprintf(log_file, "[%3lu/%3lu/%3lu/%3lu/%3lu] ", secs / 86400, (secs / 3600) % 24, secs % 3600, (t / 1000000) % 1000, (t / 1000) % 1000);
   va_list args;
   va_start(args, console_log);
   vfprintf(log_file, fmt, args);
